1f2825fba42ae127d0369468691eaefb64c30d6a
Fares Karim <faressskarim@gmail.com>
2023-04-16 16:05:23 +0200

    Update

--- Task/src/main/java/Java/Rest/CalculatorAPI.java
+++ Task/src/main/java/Java/Rest/CalculatorAPI.java
@@ -4,15 +4,8 @@ import java.util.*;
 import javax.ejb.*;
 import Java.models.Calculations;
 import Java.Service.*;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 
 @Stateless
 @ApplicationPath("/api")
