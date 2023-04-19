3a58119672f14e22be523764e96ab4c54a313351
Fares Karim <faressskarim@gmail.com>
2023-04-19 16:00:42 +0200

    Updated attribute names

--- Task/src/main/java/Java/Models/Calculations.java
+++ Task/src/main/java/Java/Models/Calculations.java
@@ -7,9 +7,9 @@ public class Calculations {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
 
-    private int number1;
-    private int number2;
-    private String operation;
+    private int Number1;
+    private int Number2;
+    private String Operation;
 
     public int getId() {
         return id;
@@ -18,21 +18,21 @@ public class Calculations {
         this.id = id;
     }
     public int getNumber1() {
-        return number1;
+        return Number1;
     }
-    public void setNumber1(int number1) {
-        this.number1 = number1;
+    public void setNumber1(int Number1) {
+        this.Number1 = Number1;
     }
     public int getNumber2() {
-        return number2;
+        return Number2;
     }
-    public void setNumber2(int number2) {
-        this.number2 = number2;
+    public void setNumber2(int Number2) {
+        this.Number2 = Number2;
     }
     public String getOperation() {
-        return operation;
+        return Operation;
     }
-    public void setOperation(String operation) {
-        this.operation = operation;
+    public void setOperation(String Operation) {
+        this.Operation = Operation;
 	}
 }
